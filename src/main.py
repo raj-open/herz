a6cb5ddb2734a92e4592e064ba05458c4bedd4bf
raj-open <raj-open@users.noreply.github.com>
2023-09-19 14:54:37 +0200

    main > main: init execution scripts+nb

---
+++ src/main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import os
+import sys
+
+os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+
+from .core.log import *
+from .setup import config
+from src.steps import *
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# LOCAL VARIABLES / CONSTANTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def enter(path: str, *_):
+    log_progress('''SETUP''', 0, 1)
+    config.set_user_config(path)
+
+    log_progress('''READ DATA''', 0, 2)
+    data_pressure, data_volume = step_read_data()
+    data = step_combine_data(data_pressure, data_volume)
+
+    log_progress('''PROCESS DATA''', 0, 2)
+    data = step_compute_extremes(data, quantities=['pressure', 'volume'])
+    # determine cycles based on pressure-peaks
+    data = step_recognise_cycles(data, quantity='pressure')
+
+    log_progress('''OUTPUT TABLES''', 0, 1)
+    step_output_tables(data)
+
+    log_progress('''OUTPUT TIME PLOTS''', 0, 1)
+    plt = step_output_time_plots(data)
+    # plt.show()
+
+    log_progress('''OUTPUT LOOP PLOTS''', 0, 1)
+    plt = step_output_loop_plot(data)
+    # plt.show()
+    return
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# SECONDARY
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# EXCEUTION
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    enter(*args)
