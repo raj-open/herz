4a28a4105f55c81451ec29d2393aef93619224f9
raj-open <raj-open@users.noreply.github.com>
2023-09-19 14:53:23 +0200

    main > main: init source models

---
+++ src/models/user/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+from ..generated.user import *
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# EXPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__all__ = [
+    'DataTimeSeries',
+    'DataTimeSeriesCommon',
+    'DataTypeQuantity',
+    'EnumLogLevel',
+    'EnumType',
+    'UserBasicOptions',
+    'UserConfig',
+    'UserData',
+    'UserOutput',
+]
