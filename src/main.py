c5636d51c22f554945ad24a2694cdc807e266e6c
raj-open <raj-open@users.noreply.github.com>
2023-09-24 18:00:55 +0200

    dev > main: use of cases in execution scripts

--- src/main.py
+++ src/main.py
@@ -29,28 +29,29 @@ def enter(path: str, *_):
     log_progress('''SETUP''', 0, 1)
     config.set_user_config(path)
 
-    log_progress('''READ DATA''', 0, 2)
-    data_pressure, data_volume = step_read_data()
-    data = step_combine_data(data_pressure, data_volume)
+    for case in config.CASES:
+        log_progress('''READ DATA''', 0, 2)
+        data_pressure, data_volume = step_read_data(case)
+        data = step_combine_data(case, data_pressure, data_volume)
 
-    log_progress('''PROCESS DATA''', 0, 2)
-    data = step_compute_extremes(data, quantities=['pressure', 'volume'])
-    data = step_recognise_cycles(data, quantity='pressure')
-    if config.PROCESS_CONFIG.cycles.remove_bad:
-        data = step_removed_marked_sections(data)
-    data = step_fit_curve(data, quantities=['pressure', 'volume'])
+        log_progress('''PROCESS DATA''', 0, 4)
+        data = step_compute_extremes(case, data, quantities=['pressure', 'volume'])
+        data = step_recognise_cycles(case, data, quantity='pressure')
+        if case.process.cycles.remove_bad:
+            data = step_removed_marked_sections(case, data)
+        data = step_fit_curve(case, data, quantities=['pressure', 'volume'])
 
-    log_progress('''OUTPUT TABLES''', 0, 1)
-    step_output_tables(data)
+        log_progress('''OUTPUT TABLES''', 0, 1)
+        step_output_tables(case, data)
 
-    log_progress('''OUTPUT TIME PLOTS''', 0, 1)
-    plt_p, plt_v = step_output_time_plots(data)
-    # plt_p.show()
-    # plt_v.show()
+        log_progress('''OUTPUT TIME PLOTS''', 0, 1)
+        plt_p, plt_v = step_output_time_plots(case, data)
+        # plt_p.show()
+        # plt_v.show()
 
-    log_progress('''OUTPUT LOOP PLOTS''', 0, 1)
-    plt = step_output_loop_plot(data)
-    # plt.show()
+        log_progress('''OUTPUT LOOP PLOTS''', 0, 1)
+        plt = step_output_loop_plot(data)
+        # plt.show()
     return
 
 
