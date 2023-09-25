afbc6ec4cefb35a1883c3e79c0a41f3781779566
raj-open <raj-open@users.noreply.github.com>
2023-09-25 14:22:31 +0200

    dev > main: added utils for integrals and norms

--- src/core/utils.py
+++ src/core/utils.py
@@ -16,6 +16,11 @@ __all__ = [
     'where_to_characteristic',
     'characteristic_to_where',
     'normalise_to_unit_interval',
+    'integral_interpolated',
+    'innerproduct_interpolated',
+    'norm_interpolated',
+    'normalise_interpolated',
+    'normalise_interpolated_drift',
 ]
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -51,3 +56,79 @@ def normalise_to_unit_interval(t: np.ndarray) -> tuple[np.ndarray, float]:
     T = t_max - t_min or 1.0
     t = (t - t_min) / T
     return t, T
+
+
+def integral_interpolated(
+    t: np.ndarray,
+    x: np.ndarray,
+    T: float,
+    periodic: bool = False,
+    average: bool = False,
+) -> float:
+    '''
+    Computes integral based on piecewise linear interpolation
+    of a discrete time-series (`t`, `x`)
+    '''
+    t = np.asarray(t.tolist() + [T])
+
+    if periodic:
+        x = np.asarray(x.tolist() + [x[0]])
+    else:
+        x1 = np.asarray(x[1:])
+        x2 = np.asarray(x[:-1])
+        x = np.concatenate([[x[0]], (x1 + x2) / 2, [x[-1]]])
+
+    dt = np.diff(t)
+    x_m = (x[:-1] + x[1:]) / 2
+    I = sum(x_m * dt)
+    if average:
+        I = I / T
+    return I
+
+
+def innerproduct_interpolated(
+    t: np.ndarray,
+    x: np.ndarray,
+    y: np.ndarray,
+    T: float,
+    periodic: bool = False,
+    average: bool = True,
+) -> float:
+    s = integral_interpolated(t, x * y, T=T, periodic=periodic, average=average)
+    return s
+
+
+def norm_interpolated(
+    t: np.ndarray,
+    x: np.ndarray,
+    T: float,
+    periodic: bool = False,
+    average: bool = True,
+) -> float:
+    s = innerproduct_interpolated(t, x, x, T=T, periodic=periodic, average=average)
+    return np.sqrt(s)
+
+
+def normalise_interpolated(
+    t: np.ndarray,
+    x: np.ndarray,
+    T: float,
+) -> tuple[float, float, np.ndarray]:
+    m = integral_interpolated(t, x, T=T, periodic=False, average=True)
+    x = x - m * (t - t[0])
+    s = norm_interpolated(t, x, T=T, periodic=False, average=True)
+    x = x / (s or 1.0)
+    return m, s, x
+
+
+def normalise_interpolated_drift(
+    t: np.ndarray,
+    x: np.ndarray,
+    T: float,
+) -> tuple[float, float, float, np.ndarray]:
+    m = (x[-1] - x[0]) / (t[-1] - t[0])
+    c = x[0] - m * t[0]
+    x = x - (c + m * t)
+    s = norm_interpolated(t, x, T=T, periodic=False, average=True)
+    x = x / (s or 1.0)
+    return c, m, s, x
