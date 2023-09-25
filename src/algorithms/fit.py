dev > main: cleaned up normalisation of polynomials - correct storage of polynomial infos - computation of mean / median aspects - added expression output

src/algorithms/fit.py
@@ -35,7 +35,7 @@ def fit_poly_cycles(
     deg: int,
     conds: list[PolyDerCondition | PolyIntCondition],
     average: bool = False,
-) -> tuple[np.ndarray, list[float]]:
+) -> list[tuple[list[float], float, float, float, float]]:
     '''
     Fits 'certain' polynomials to cycles in such a way,
     that special attributes can be extracted.
@@ -46,20 +46,19 @@ def fit_poly_cycles(
     # fit each cycle
     N = len(t)
     x_fit = np.zeros(shape=(N,), dtype=float)
-    coeffs = [[]] * len(windows)
+    infos = [[]] * len(windows)
     # due to normalisation, force the following condition
     conds = conds[:]
     conds.append(PolyDerCondition(derivative=0, time=0))
     conds.append(PolyDerCondition(derivative=0, time=1))
     for k, (i1, i2) in enumerate(windows):
         # scale time
-        tt, _ = normalise_to_unit_interval(t[i1:i2])
+        tt, T = normalise_to_unit_interval(t[i1:i2])
         # remove drift
         c, m, s, xx = normalise_interpolated_drift(tt, x[i1:i2], T=1)
         # compute fitted curve
-        xx_fit, coeffs[k] = fit_poly_cycle(t=tt, x=xx, deg=deg, conds=conds)
-        # undo effects of drift-removal
-        x_fit[i1:i2] = c + m * tt + s * xx_fit
+        coeff = fit_poly_cycle(t=tt, x=xx, deg=deg, conds=conds)
+        infos[k] = (coeff, T, c, m, s)
 
     # --------------------------------
     # NOTE:
@@ -93,20 +92,14 @@ def fit_poly_cycles(
     # of the coefficients of the p⁽ᵏ⁾.
     # --------------------------------
     if average:
-        coeff = np.mean(np.asarray(coeffs), axis=0).tolist()
-        for k, (i1, i2) in enumerate(windows):
-            # scale time
-            tt, _ = normalise_to_unit_interval(t[i1:i2])
-            # remove drive
-            c, m, s, _ = normalise_interpolated_drift(tt, x[i1:i2], T=1)
-            # compute fitted curve
-            xx_fit = poly(tt, *coeff)
-            # undo effects of drift-removal
-            x_fit[i1:i2] = c + m * tt + s * xx_fit
+        coeff_ = np.mean(np.asarray([coeff for coeff, _, _, _, _ in infos]), axis=0).tolist()
+        T_ = np.median(np.asarray([T for _, T, _, _, _ in infos]), axis=0).tolist()
+        c_ = np.median(np.asarray([c for _, _, c, _, _ in infos]), axis=0).tolist()
+        m_ = np.median(np.asarray([m for _, _, _, m, _ in infos]), axis=0).tolist()
+        s_ = np.median(np.asarray([s for _, _, _, _, s in infos]), axis=0).tolist()
+        infos.append((coeff_, T_, c_, m_, s_))
 
-        coeffs = [coeff]
-
-    return x_fit, coeffs
+    return infos
 
 
 def fit_poly_cycle(
@@ -114,7 +107,7 @@ def fit_poly_cycle(
     x: np.ndarray,
     deg: int,
     conds: list[PolyDerCondition | PolyIntCondition],
-) -> tuple[np.ndarray, list[float]]:
+) -> list[float]:
     '''
     Fits 'certain' polynomials to a cycle in such a way,
     that special attributes can be extracted.
@@ -132,9 +125,7 @@ def fit_poly_cycle(
     '''
     Q = onb_conditions(deg=deg, conds=conds)
     coeff = onb_spectrum(t=t, x=x, Q=Q, T=1, in_standard_basis=True)
-    x_fit = poly(t, *coeff)
-
-    return x_fit, coeff
+    return coeff
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

src/steps/step_process_data.py
@@ -92,16 +92,7 @@ def step_recognise_cycles(
     data['time[orig]'] = data['time']
     data['time'] = np.linspace(start=0.0, stop=T_max, num=N, endpoint=True)
 
-    # 6. compute period
-    cycles = data['cycle'].tolist()
-    t = data['time'].tolist() + [T_max]
-    windows = cycles_to_windows(cycles)
-    if len(windows) > 0:
-        T = np.median([t[i2] - t[i1] for i1, i2 in windows])
-    else:
-        T = t_max
-
-    return data, T
+    return data
 
 
 def step_removed_marked_sections(
@@ -131,7 +122,7 @@ def step_fit_curve(
     data: pd.DataFrame,
     quantity: str,
     n_der: int = 2,
-) -> tuple[pd.DataFrame, list[float]]:
+) -> tuple[pd.DataFrame, list[tuple[list[float], float, float, float, float]]]:
     '''
     Fits polynomial to cycles in time-series,
     forcing certain conditions on the `n`th-derivatives
@@ -150,7 +141,7 @@ def step_fit_curve(
     # fit polynomial
     x = data[quantity].to_numpy(copy=True)
     mode_average = cfg.fit.mode == EnumFittingMode.AVERAGE
-    x, coeffs = fit_poly_cycles(
+    infos = fit_poly_cycles(
         t=t,
         x=x,
         cycles=cycles,
@@ -158,23 +149,41 @@ def step_fit_curve(
         conds=cfg_poly.conditions,
         average=mode_average,
     )
-    data[f'{quantity}[fit]'] = x
 
     # compute derivatives
-    for n in range(1, n_der + 1):
+    if mode_average:
+        coeff, _, _, _, _ = infos[-1]
+        coeffs = [coeff[:] for _ in windows]
+    else:
+        coeffs = [coeff[:] for coeff, _, _, _, _ in infos]
+    for n in range(n_der + 1):
+        # loop over all time-subintervals:
         for k, (i1, i2) in enumerate(windows):
-            if not mode_average or k == 0:
-                coeffs[k] = derivative_coefficients(coeffs[k])
-            coeff = coeffs[0] if mode_average else coeffs[k]
+            # get coefficients for (n-1)th derivative polynomial for cycle k:
+            coeff = coeffs[k]
+            # get drift-values:
+            _, _, c, m, s = infos[k]
             # scale time
             tt, T = normalise_to_unit_interval(t[i1:i2])
-            # remove drift
-            c, m, s, _ = normalise_interpolated_drift(tt, x[i1:i2], T=1)
-            # compute normalised derivative
-            dnx = poly(tt, *coeff)
-            # undo effects of time-scaling + drift-removal
-            x[i1:i2] = ((m if n == 1 else 0.0) + s * dnx) / T
+            # compute nth-derivative of fitted polynom to normalise cycle
+            if n > 0:
+                coeff = derivative_coefficients(coeff)
+                coeffs[k] = coeff
+            xx = poly(tt, *coeff)
+            # undo effects of time-scaling and drift-removal
+            # NOTE: from 2nd derivative onwards, drift-removal has no effect)
+            match n:
+                case 0:
+                    x[i1:i2] = c + m * tt + s * xx
+                case 1:
+                    x[i1:i2] = (m + s * xx) / T
+                case _:
+                    x[i1:i2] = s * xx / T
+        # store fitted results
+        match n:
+            case 0:
+                data[f'{quantity}[fit]'] = x
+            case _:
+                data[f'd[{n},t]{quantity}[fit]'] = x
 
-        data[f'd[{n},t]{quantity}[fit]'] = x
-
-    return data, coeffs[0]
+    return data, infos