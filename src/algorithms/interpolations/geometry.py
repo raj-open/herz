dev > main: corrected interpolation + normalisation

src/algorithms/interpolations/geometry.py
@@ -23,11 +23,10 @@ __all__ = [
 
 
 def integral_interpolated(
-    t: np.ndarray,
-    x: np.ndarray,
+    t: NDArray[np.float64],
+    x: NDArray[np.float64],
     T: float,
     periodic: bool = False,
-    average: bool = False,
 ) -> float:
     '''
     Computes integral based on piecewise linear interpolation
@@ -45,26 +44,26 @@ def integral_interpolated(
     dt = np.diff(t)
     x_m = (x[:-1] + x[1:]) / 2
     I = sum(x_m * dt)
-    if average:
-        I = I / T
     return I
 
 
 def innerproduct_interpolated(
-    t: np.ndarray,
-    x: np.ndarray,
-    y: np.ndarray,
+    t: NDArray[np.float64],
+    x: NDArray[np.float64],
+    y: NDArray[np.float64],
     T: float,
     periodic: bool = False,
     average: bool = True,
 ) -> float:
-    s = integral_interpolated(t, x * y, T=T, periodic=periodic, average=average)
+    s = integral_interpolated(t, x * y.conjugate(), T=T, periodic=periodic)
+    if average:
+        s = s / T
     return s
 
 
 def norm_interpolated(
-    t: np.ndarray,
-    x: np.ndarray,
+    t: NDArray[np.float64],
+    x: NDArray[np.float64],
     T: float,
     periodic: bool = False,
     average: bool = True,

src/algorithms/interpolations/normalisation.py
@@ -7,9 +7,8 @@
 
 from ...thirdparty.code import *
 from ...thirdparty.maths import *
-from ...thirdparty.types import *
 
-from .geometry import *
+from .statistics import *
 
 # ----------------------------------------------------------------
 # EXPORTS
@@ -17,7 +16,7 @@ from .geometry import *
 
 __all__ = [
     'normalise_to_unit_interval',
-    'normalise_interpolated_drift',
+    'normalise_interpolated_cycle',
 ]
 
 # ----------------------------------------------------------------
@@ -25,7 +24,9 @@ __all__ = [
 # ----------------------------------------------------------------
 
 
-def normalise_to_unit_interval(t: np.ndarray) -> tuple[np.ndarray, float]:
+def normalise_to_unit_interval(
+    t: NDArray[np.float64],
+) -> tuple[NDArray[np.float64], float]:
     t_min = min(t)
     t_max = max(t)
     T = t_max - t_min
@@ -33,19 +34,33 @@ def normalise_to_unit_interval(t: np.ndarray) -> tuple[np.ndarray, float]:
     return t, T
 
 
-def normalise_interpolated_drift(
-    t: np.ndarray,
-    x: np.ndarray,
+def normalise_interpolated_cycle(
+    t: NDArray[np.float64],
+    x: NDArray[np.float64],
     T: float,
     periodic: bool = False,
-) -> tuple[float, float, float, np.ndarray]:
-    m = (x[-1] - x[0]) / (t[-1] - t[0])
+) -> tuple[float, float, float, NDArray[np.float64]]:
+    # ensure time axis starts at 0
+    t = t - t[0]
+
+    # 1. remove linear trend
+    # NOTE: disable drift correction
+    # m = (x[-1] - x[0]) / (t[-1] - t[0])
     m = 0
-    c = x[0] - m * t[0]
-    x = x - (c + m * t)
-    x_max = np.max(abs(x))
-    x = x / (x_max or 1.0)
-    s = norm_interpolated(t, x, T=T, periodic=periodic, average=True)
-    x = x / (s or 1.0)
-    s = s * x_max
+    x = x - m * t
+
+    # 2. normalise oscillation
+    mu = mean_interpolated(t, x, T=T, periodic=periodic)
+    sd = sd_interpolated(t, x, x_mean=mu, T=T, periodic=periodic)
+    ampl = math.sqrt(2) * sd
+    x = (x - mu) / (ampl or 1.0)
+
+    # 3. remove initial offset
+    c = x[0]
+    x = x - c
+
+    # 4. correct constants, so that x_orig = c + m * t + s * x
+    c = mu + ampl * c
+    s = ampl
+
     return c, m, s, x

src/queries/fitting/normalisation.py
@@ -17,39 +17,24 @@ from ...models.polynomials import *
 # ----------------------------------------------------------------
 
 __all__ = [
-    'get_normalisation_params',
-    'get_normalised_data',
     'get_unnormalised_data',
-    'get_unnormalised_fit_trig',
+    'get_unnormalised_trig',
     'get_unnormalised_point',
     'get_unnormalised_polynomial',
-    'get_unnormalised_polynomial_time_only',
-    'get_unnormalised_polynomial_values_only',
     'get_unnormalised_special',
     'get_unnormalised_time',
 ]
 
 # ----------------------------------------------------------------
-# METHODS - ONB
+# METHODS
 # ----------------------------------------------------------------
-
-
-def get_normalisation_params(
-    info: FittedInfo,
-) -> list[float, float, float, float]:
-    params = info.normalisation
-    T = params.period
-    c = params.intercept
-    m = params.gradient
-    s = params.scale
-    return T, c, m, s
 
 
 def get_unnormalised_time(
     t: float,
-    info: FittedInfo,
+    info: FittedInfoNormalisation,
 ) -> float:
-    T = info.normalisation.period
+    T = info.period
     t = T * (t % 1)
     return t
 
@@ -57,7 +42,7 @@ def get_unnormalised_time(
 def get_unnormalised_point(
     t: float,
     x: float,
-    info: FittedInfo,
+    info: FittedInfoNormalisation,
 ) -> tuple[float, float]:
     T, c, m, s = get_normalisation_params(info)
     t = T * (t % 1)
@@ -66,8 +51,9 @@ def get_unnormalised_point(
 
 
 def get_unnormalised_polynomial(
-    info: FittedInfo,
-) -> Poly:
+    fit: FittedInfoPoly,
+    info: FittedInfoNormalisation,
+) -> Poly[float]:
     '''
     Normalisation:
     ```
@@ -78,174 +64,25 @@ def get_unnormalised_polynomial(
     x(t) = c + m/T · (t-t₀) + s · z((t-t₀)/T)
     ```
     '''
-    coeff = info.coefficients
+    coeff = fit.coefficients
+    p = Poly[float](coeff=coeff)
     T, c, m, s = get_normalisation_params(info)
-    deg = len(coeff) - 1
-    Tpow = np.cumprod([1] + [T] * deg)
-    coeff_rescaled = [s * cc / TT for cc, TT in zip(coeff, Tpow)]
-    coeff_rescaled[0] += c
-    coeff_rescaled[1] += m / T
-    return Poly(coeff=coeff_rescaled)
+    return Poly[float](coeff=[c, m]) + s * p.rescale(a=1 / T)
 
 
-def get_unnormalised_polynomial_time_only(
-    info: FittedInfo,
-) -> Poly:
-    coeff = info.coefficients
-    T, c, m, s = get_normalisation_params(info)
-    deg = len(coeff) - 1
-    Tpow = np.cumprod([1] + [T] * deg)
-    coeff_rescaled = [s * cc / TT for cc, TT in zip(coeff, Tpow)]
-    return Poly(coeff=coeff_rescaled)
-
-
-def get_unnormalised_polynomial_values_only(
-    info: FittedInfo,
-) -> Poly:
-    '''
-    Normalisation:
-    ```
-    z(t) = (x(t₀ + T·t) - (c + mt))/s
-    ```
-    Unnormalisation that restores the shape of the the polynomial only,
-    but does not re-scale time and values.
-    ```
-    x(T·t)/s = c/s + m/s · (t – t₀) + z(t – t₀)
-    ```
-    '''
-    coeff = info.coefficients
-    T, c, m, s = get_normalisation_params(info)
-    coeff_rescaled = coeff[:]
-    coeff_rescaled[0] += c / (s or 1.0)
-    coeff_rescaled[1] += m / (s or 1.0)
-    return Poly(coeff=coeff_rescaled)
-
-
-def get_unnormalised_data(
-    data: pd.DataFrame,
-    infos: list[tuple[tuple[int, int], FittedInfo]],
-    quantity: str,
-    t_split: float = 0.0,
-    renormalise: bool = True,
-) -> pd.DataFrame:
-    '''
-    Undoes normalisation of the data series.
-    If `renormalise=True` is set,
-    then renormalises using commmon parameters.
-    '''
-    t_new = []
-    dt_new = []
-    x_new = []
-    t = data['time'].to_numpy(copy=True)
-    x = data[quantity].to_numpy(copy=True)
-
-    # get common parameters
-    if renormalise:
-        _, info = infos[-1]
-        T0, c0, m0, s0 = get_normalisation_params(info)
-    else:
-        T0, c0, m0, s0 = 1, 0, 0, 1
-
-    # un- and renormalise all cycles
-    for (i1, i2), info in infos[:-1]:
-        T, c, m, s = get_normalisation_params(info)
-        # normalise time and points for cycle
-        # NOTE: normalise t, then x!
-        tt = (t[i1:i2] - t[i1]) / T
-        xx = x[i1:i2]
-        xx = (xx - (c + m * tt)) / s
-        # renormalise based on common parameters
-        # NOTE: renormalise x, then t!
-        xx = c0 + m0 * tt + s0 * xx
-        tt = T0 * tt
-        # determine time-increments
-        dt = np.diff(tt)
-        dt = np.concatenate([dt, [dt[-1]]])
-        # store
-        t_new += tt.tolist()
-        dt_new += dt.tolist()
-        x_new += xx.tolist()
-
-    # split times
-    t_new = np.asarray(t_new)
-    t = t_new + T0 * (t_new < t_split) - t_split
-
-    # store in new data structure
-    data = pd.DataFrame(
-        {
-            'time[orig]': t_new,
-            'time': t,
-            'dt': dt_new,
-            quantity: x_new,
-        }
-    ).astype(
-        {
-            'time[orig]': 'float',
-            'time': 'float',
-            'dt': 'float',
-            quantity: 'float',
-        }
-    )
-    return data
-
-
-def get_normalised_data(
-    data: pd.DataFrame,
-    infos: list[tuple[tuple[int, int], FittedInfo]],
-    quantity: str,
-    t_split: float = 0.0,
-) -> pd.DataFrame:
-    '''
-    Normalises data series.
-    '''
-    data = get_unnormalised_data(
-        data,
-        infos=infos,
-        quantity=quantity,
-        t_split=t_split,
-        renormalise=False,
-    )
-    data = data[['time', 'dt', quantity]]
-    data = data.rename(columns={quantity: 'value'})
-    return data
-
-
-def get_unnormalised_special(
-    special: dict[str, SpecialPointsConfig],
-    info: FittedInfo,
-    key_align: str,
-):
-    '''
-    Renormalises (without realignment) + adds the alignment time.
-    '''
-    point = special[key_align]
-    t_align = get_unnormalised_time(point.time, info=info)
-    for _, point in special.items():
-        t, x = point.time, point.value
-        t, x = get_unnormalised_point(t, x, info=info)
-        point.time = t
-        point.value = x
-    special['align'] = SpecialPointsConfig(
-        name='align',
-        ignore=True,
-        time=t_align,
-    )
-    return special
-
-
-def get_unnormalised_fit_trig(
+def get_unnormalised_trig(
     fit: FittedInfoTrig,
-    info: FittedInfo,
+    info: FittedInfoNormalisation,
 ) -> FittedInfoTrig:
     '''
     The model
     ```
-    f(t) = a + b·(t - t₀) + r·cos(ω·t - φ)
+    f(t) = a + b·t + r·cos(ω·(t - t₀))
     ```
-    is first unnormalised to
+    is unnormalised to
     ```
-    g(t) = C + m·t + f(t)
-        = (C + s·a) + m
+    g(t) = C + m·t + s·f(t/T)
+         = (C + s·a) + (m + s·b/T)·t + s·r·cos(ω/T·(t - t₀·T))
     ```
     '''
     T, c, m, s = get_normalisation_params(info)
@@ -254,5 +91,84 @@ def get_unnormalised_fit_trig(
         hscale=T * fit.hscale,
         vshift=c + s * fit.vshift,
         vscale=s * fit.vscale,
-        drift=m + s * fit.drift,
+        # NOTE: this remains 0 if m and fit.drift are 0
+        drift=m + s * fit.drift / T,
     )
+
+
+def get_unnormalised_data(
+    data: pd.DataFrame,
+    infos: list[tuple[tuple[int, int], FittedInfoNormalisation]],
+    quantity: str,
+    n_der: int = 0,
+    renormalise: bool = True,
+) -> pd.DataFrame:
+    '''
+    Undoes normalisation of the data series.
+    If `renormalise=True` is set,
+    then renormalises using commmon parameters.
+    '''
+    data['dt'] = 0
+
+    # get common parameters
+    _, fit0 = infos[-1]
+
+    # un/renormalise all cycles - incl. fitted models
+    for (i1, i2), fit in infos[:-1]:
+        T, c, m, s = get_normalisation_params(fit0 if renormalise else fit)
+        tt = data['time'][i1:i2]
+        xx = data[quantity][i1:i2]
+
+        tt = T * tt
+        xx = c + m * tt + s * xx
+
+        data['time'][i1:i2] = tt
+        data[quantity][i1:i2] = xx
+
+        for n in range(n_der + 1):
+            match n:
+                case 0:
+                    col = f'{quantity}[fit]'
+                    xx = data[col][i1:i2]
+                    xx = c + m * tt + s * xx
+                case 1:
+                    col = f'd[{n},t]{quantity}[fit]'
+                    xx = data[col][i1:i2]
+                    xx = m + s * xx
+                case _:
+                    col = f'd[{n},t]{quantity}[fit]'
+                    xx = data[col][i1:i2]
+                    xx = s * xx
+            data[col][i1:i2] = xx
+
+    return data
+
+
+def get_unnormalised_special(
+    special: dict[str, SpecialPointsConfig],
+    info: FittedInfoNormalisation,
+):
+    '''
+    Renormalises (without realignment) + adds the alignment time.
+    '''
+    for _, point in special.items():
+        t, x = point.time, point.value
+        t, x = get_unnormalised_point(t, x, info=info)
+        point.time = t
+        point.value = x
+    return special
+
+
+# ----------------------------------------------------------------
+# AUXILIARY METHODS
+# ----------------------------------------------------------------
+
+
+def get_normalisation_params(
+    info: FittedInfoNormalisation,
+) -> tuple[float, float, float, float]:
+    T = info.period
+    c = info.intercept
+    m = info.gradient
+    s = info.scale
+    return T, c, m, s