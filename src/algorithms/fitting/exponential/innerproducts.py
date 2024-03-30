dev > main: bugfix - corrected computation of inner prod
error was
```py
ip['f*exp'] = np.sum((x * E) * dt)
```
this should have been
```py
ip['f*exp'] = np.sum((y * E) * dt)
```

src/algorithms/fitting/exponential/innerproducts.py
@@ -35,18 +35,20 @@ def compute_inner_products_from_data(
     Thus need inner products in order to compute
     L²-loss function + gradients.
     '''
-    t, dt, x = data[:, 0], data[:, 1], data[:, 2]
-    E = np.exp(beta * t)
-    E2 = np.exp(2 * beta * t)
+    dt, x, y = data[:, 1], data[:, 2], data[:, -1]
+    ones = np.ones(x.shape)
+    E = np.exp(beta * x)
+    E2 = E**2
+    y2 = y**2
 
     return {
-        '1': np.sum(dt),
+        '1': np.sum(ones * dt),
         'exp': np.sum(E * dt),
         'exp^2': np.sum(E2 * dt),
-        'f': np.sum(x * dt),
-        'f*exp': np.sum((x * E) * dt),
-        'f^2': np.sum((x**2) * dt),
-        't*exp': np.sum(t * E * dt),
-        't*exp^2': np.sum(t * E2 * dt),
-        't*f*exp': np.sum(t * x * E * dt),
+        'f': np.sum(y * dt),
+        'f*exp': np.sum((y * E) * dt),
+        'f^2': np.sum(y2 * dt),
+        't*exp': np.sum((x * E) * dt),
+        't*exp^2': np.sum((x * E2) * dt),
+        't*f*exp': np.sum((x * y * E) * dt),
     }