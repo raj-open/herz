515755bb3d8eb6546a746fbf2f80897bc5065468
raj-open <raj-open@users.noreply.github.com>
2024-03-30 03:25:57 +0100

    dev > main: minor

--- src/algorithms/interpolations/linear.py
+++ src/algorithms/interpolations/linear.py
@@ -29,6 +29,7 @@ def interpolate_two_series(
     NDArray[np.float64],
     NDArray[np.float64],
     NDArray[np.float64],
+    NDArray[np.float64],
 ]:
     '''
     Takes two time-series defined on `[0, 1)`
@@ -36,6 +37,9 @@ def interpolate_two_series(
     '''
     t = sorted(set(t1.tolist() + t2.tolist()))
     t = np.asarray(t)
+    dt = np.diff(t)
+    dt = np.concatenate([dt[:1], dt, dt[-1:]])
+    dt = (dt[:-1] + dt[1:]) / 2
     x1 = np.interp(t, xp=t1, fp=x1, left=x1[0], right=x1[-1])
     x2 = np.interp(t, xp=t2, fp=x2, left=x2[0], right=x2[-1])
-    return t, x1, x2
+    return t, dt, x1, x2
