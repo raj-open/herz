6d0e5b8a3366e2afeacc38022fa52e261ada51c3
raj-open <raj-open@users.noreply.github.com>
2024-04-01 21:09:18 +0200

    dev > main: minor

--- src/thirdparty/web.py
+++ src/thirdparty/web.py
@@ -23,7 +23,7 @@ class MimeType(Enum):
     JSON = 'application/json;charset=utf-8'
     # see https://learn.microsoft.com/previous-versions/office/office-2007-resource-kit/ee309278(v=office.12)
     XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    YAML = 'application/x-yaml'
+    YAML = 'application/yaml;charset=utf-8'
 
 
 # ----------------------------------------------------------------
